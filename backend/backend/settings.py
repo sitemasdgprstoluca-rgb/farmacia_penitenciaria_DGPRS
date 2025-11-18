9080d087b276d2c8292ed8b6b63ec186f8476b72
Jael Alexander <zaragozaalexander124@gmail.com>
2025-11-18 17:33:34 -0600

    Agregar dominio de Render a ALLOWED_HOSTS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,7 +10,8 @@ SECRET_KEY = 'django-insecure-tu-clave-secreta-cambiar-en-produccion'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'farmacia-penitenciaria.onrender.com']
 
 # Application definition
 INSTALLED_APPS = [
